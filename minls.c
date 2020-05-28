7301d7ff9c71d9c05e9403fe5c41940eda359574
FelixSitu <chsitu@calpoly.edu>
2020-05-27 22:35:19 -0700

    minls has p and s case condition

--- minls.c
+++ minls.c
@@ -10,5 +10,20 @@ int main(int argc, char *argv[]){
 
 	printf("This is opt: %i\n", opt);
 
+	if (opt == -1){
+		printf("Bad-case\n");
+		return 0;
+	}
+	else if (opt == 'p'){
+		printf("P-case\n");
+	}
+	else if (opt == 's'){
+		printf("S-case\n");
+	}
+	else{
+		printf("NO-case\n");
+		return 0;
+	}
+
   return 0;
 }
