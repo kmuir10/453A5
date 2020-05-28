146ce5e5185ad487802dfb33c3b27e4f3f02e6cb
FelixSitu <chsitu@calpoly.edu>
2020-05-27 21:23:06 -0700

    Revert "Merge branch 'master' of https://github.com/kmuir10/453A5"
    This reverts commit e6cb99aeca52f95ed6c554e84078bfb8a9521f18, reversing
    changes made to 49a05b08562f0dafbd32955f0c9087c340f0b09d.

--- mintool.c
+++ mintool.c
@@ -1,14 +1,9 @@
 #include "mintool.h"
 
-int32_t swend32(int32_t i){
-  return (((i&0xFF)<<24)|((i&0xFF00)<<8)|
-      ((i&0xFF0000)>>8)|((i&0xFF000000)>>24));
+int32_t liend32(int32_t i){
+  return (((i&0xFF)<<24)|((i&0xFF00)<<8)|((i&0xFF0000)>>8)|((i&0xFF000000)>>24));
 }
 
-int16_t swend16(int16_t i){
+int16_t liend16(int16_t i){
   return (((i&0xFF)<<8)|((i&0xFF00)>>8));
-}
-
-int32_t zsize(Sblock sblk){
-  return sblk->blocksize << log_zone_size;
 }
