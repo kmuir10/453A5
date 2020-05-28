11fab1dab14f9944e612dad8cafcd25d99e3c46b
FelixSitu <chsitu@calpoly.edu>
2020-05-27 22:51:10 -0700

    minls has better getopt

--- minls.c
+++ minls.c
@@ -6,23 +6,25 @@
 
 int main(int argc, char *argv[]){
   
-	int opt = getopt(argc, argv, "p:s");
+	int opt = 0;
 
 	printf("This is opt: %i\n", opt);
 
-	if (opt == -1){
-		printf("Bad-case\n");
-		return 0;
-	}
-	else if (opt == 'p'){
-		printf("P-case\n");
-	}
-	else if (opt == 's'){
-		printf("S-case\n");
-	}
-	else{
-		printf("NO-case\n");
-		return 0;
+	while ((opt = getopt(argc, argv, "p:s")) != -1){
+		if (opt == -1){
+			printf("Bad-case\n");
+			return 0;
+		}
+		else if (opt == 'p'){
+			printf("P-case\n");
+		}
+		else if (opt == 's'){
+			printf("S-case\n");
+		}
+		else{
+			printf("NO-case\n");
+			return 0;
+		}
 	}
 
   return 0;
