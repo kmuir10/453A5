21c28fc2577d4688f0ea327298b06054d08316c7
Kyle Muir <kylemuir567@gmail.com>
2020-05-28 21:49:51 -0700

    some changes to minget.c, not sure what lol

--- minget.c
+++ minget.c
@@ -6,6 +6,7 @@
 int main(int argc, char *argv[]){
   char *env = getenv("MY_DEBUG");
   char b510, b511;
+  size_t sz = 0;
   Partn pt = malloc(sizeof(struct partn));
   eprintf("%d minget\n", 0);
   FILE *img = fopen("/home/kmuir/os/assignments/453A5/Images/Partitioned", "r");
@@ -22,9 +23,12 @@ int main(int argc, char *argv[]){
   b510 = fgetc(img);
   b511 = fgetc(img);
   printf("%d, %d\n", (int8_t)b510, (int8_t)b511);
-  pt->bootind = pt->end_cyl = 1;
-  printf("%x %x %x %x %x %x %x %x %x %x", pt->bootind, pt->start_head, pt->start_sec, pt->start_cyl, pt->type, pt->end_head, pt->end_sec, pt->end_cyl, pt->lFirst, pt->size);
-  fread(pt, 1, sizeof(struct partn), img);
-  printf("%x %x %x %x %x %x %x %x %x %x", pt->bootind, pt->start_head, pt->start_sec, pt->start_cyl, pt->type, pt->end_head, pt->end_sec, pt->end_cyl, pt->lFirst, pt->size);
+  if(fseek(img, 0x1BE, SEEK_SET) != 0){
+    perror("fseek");
+    exit(EXIT_FAILURE);
+  }
+  sz = fread(pt, sizeof(struct partn), 1, img);
+  eprintf("bytes read: %d\n", (int)(sz * sizeof(struct partn)));
+  printf("%x %x %x %x %x %x %x %x %x %x\n", pt->bootind, pt->start_head, pt->start_sec, pt->start_cyl, pt->type, pt->end_head, pt->end_sec, pt->end_cyl, pt->lFirst, pt->size);
   return 0;
 }
